9132af0be94728c113e2b7f15f819258d3d52e21
Akanimo Michael Asuquo <101297309+Akanimomichael@users.noreply.github.com>
2023-09-15 21:37:22 +0100

    Building to-do list

---
+++ To-do list/build.js
@@ -0,0 +1,16 @@
+const fs = require("fs-extra");
+const path = require("path");
+
+// Define the source and destination directories
+const srcDir = path.join(__dirname, "src");
+const distDir = path.join(__dirname, "dist");
+
+// Create the "dist" directory if it doesn't exist
+fs.ensureDirSync(distDir);
+
+// Copy HTML, JS, and CSS files to the "dist" directory
+fs.copySync(path.join(srcDir, "index.html"), path.join(distDir, "index.html"));
+fs.copySync(path.join(srcDir, "main.js"), path.join(distDir, "main.js"));
+fs.copySync(path.join(srcDir, "styles.css"), path.join(distDir, "styles.css"));
+
+console.log("Build completed.");
